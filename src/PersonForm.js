0d02a38c206bf473f36eef283f2fa704d1973a75
Roman <romchikus1987@gmail.com>
2023-05-18 18:06:10 +0300

    upd

--- src/PersonForm.js
+++ src/PersonForm.js
@@ -1,46 +1,46 @@
-import React from 'react';
+import React from 'react'
 
 const PersonForm = (props) => {
   let { addPerson, newNumber, handleNameChange, handleNumberChange, newName } =
-    props;
-
+    props
+  console.log('newNumber', newNumber)
   return (
     <form onSubmit={addPerson}>
-      <div className="row">
-        <div className="col mb-3">
-          <label htmlFor="name" className="form-label">
+      <div className='row'>
+        <div className='col mb-3'>
+          <label htmlFor='name' className='form-label'>
             Name:
           </label>
           <input
             value={newName}
-            placeholder="Enter name"
-            name="name"
-            id="name"
+            placeholder='Enter name'
+            name='name'
+            id='name'
             onChange={handleNameChange}
-            className="form-control"
+            className='form-control'
           />
           {''}
         </div>
-        <div className="col mb-3">
-          <label htmlFor="number" className="form-label">
+        <div className='col mb-3'>
+          <label htmlFor='number' className='form-label'>
             Number:
           </label>
           <input
-            type="number"
+            type='number'
             value={newNumber}
-            id="number"
-            placeholder="Enter number"
-            name="number"
+            id='number'
+            placeholder='Enter number'
+            name='number'
             onChange={handleNumberChange}
-            className="form-control"
+            className='form-control'
           />
         </div>
       </div>
-      <button type="submit" className="btn btn-success">
+      <button type='submit' className='btn btn-success'>
         add
       </button>
     </form>
-  );
-};
+  )
+}
 
-export default PersonForm;
+export default PersonForm
